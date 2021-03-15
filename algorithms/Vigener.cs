15fb3eab89ce5f4730240bb1e8f477d4f74c9644
Panlak <55487154+Panlak@users.noreply.github.com>
2021-03-15 12:20:49 +0300

    Update Vigener.cs

--- algorithms/Vigener.cs
+++ algorithms/Vigener.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace Wiegener_code
@@ -11,41 +8,67 @@ namespace Wiegener_code
 		static void Main(string[] args)
 		{
 			System.Console.OutputEncoding = Encoding.GetEncoding(65001);
+		
 
 
-			Console.WriteLine("Enter Key");
-			string key = Console.ReadLine();
+
 			Console.WriteLine("Enter text");
 			string text = Console.ReadLine();
 
+			Console.WriteLine("Ведідь кілість ключів");
+			int KeyLength = int.Parse( Console.ReadLine());		
+			string[] key = new string[KeyLength];
+
+			Console.WriteLine("Enter Key");
+			for (int i = 0; i < key.Length; i++)
+			{
+				key[i] = Console.ReadLine();				
+			}
+
+			string keyresult = "";
+
+			int l = -1;
+			while (keyresult.Length < text.Length)
+			{
+				
+				if (l < key.Length - 1)
+				{
+					l++;
+					
+				}
+				else
+				{
+					l--;
+				}
+				keyresult += key[l];
+			}		
+			keyresult = keyresult.Substring(0, text.Length);
+
+
 
 			Console.WriteLine("Encripted");
-			
+
 			int modul = 123;
-			while(key.Length < text.Length)
-			{
-				key += key;
-			}	
-			key = key.Substring(0, text.Length);
+
 
 			int[] Encripted = new int[text.Length];
 			int[] Decripted = new int[text.Length];
-			
+
 			for (int i = 0; i < Encripted.Length; i++)
 			{
-				Encripted[i] = (key.ToCharArray()[i] + text.ToCharArray()[i]) % modul;
+				Encripted[i] = (keyresult.ToCharArray()[i] + text.ToCharArray()[i]) % modul;
 
 				Console.Write((char)Encripted[i] + " ");
 
-				
-				
+
+
 			}
 			Console.WriteLine();
 			Console.WriteLine("Decripted");
-			
+
 			for (int i = 0; i < Decripted.Length; i++)
 			{
-				Decripted[i] = Encripted[i] + modul - key[i] ;
+				Decripted[i] = Encripted[i] + modul - keyresult.ToCharArray()[i];
 
 				Console.Write((char)Decripted[i]);
 
@@ -53,7 +76,7 @@ namespace Wiegener_code
 			Console.WriteLine();
 
 
-			
+
 		}
 	}
 }
